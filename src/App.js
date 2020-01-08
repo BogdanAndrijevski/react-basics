82bec89952b13609f8dae9bd97533854b427869e
unknown <bogdanlikuliks@hotmail.com>
2020-01-08 19:11:25 +0100

    added person component - ver 1.01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <Person>kako e</Person>
       </div>
     );
   }
 }
 
 export default App;
+
+// ver 1.01 - tut 32. Creating a Functional Component
+ 
