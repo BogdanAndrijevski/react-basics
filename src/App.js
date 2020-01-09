a910b51b8ef7a82aaaf1a6953f980f9b0e144dbd
unknown <bogdanlikuliks@hotmail.com>
2020-01-09 10:02:27 +0100

    ver 1.05 - using state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'John', age : 26},
+      { name: 'Jack', age : 46},
+      { name: 'Michael', age : 35}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <Person name="John" age="26">My Hobby: </Person>
-        <Person name="Jack" age="46"/>
-        <Person name="Michael" age="35"/>
+        <button>Switch Name</button>
+        <Person name={this.persons[0].name} age={this.persons[0].age}>My Hobby: </Person>
+        <Person name={this.persons[1].name} age={this.persons[1].age}/>
+        <Person name={this.persons[2].name} age={this.persons[2].age}/>
        
       </div>
     );
@@ -18,5 +28,5 @@ class App extends Component {
 
 export default App;
 
-// ver 1.04 - tut 37. Understanding the Children Property
+// ver 1.05 - 38. Understanding & Using State
  
