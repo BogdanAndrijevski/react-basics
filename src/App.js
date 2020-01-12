ff1074c4dd6ecc9192b0e8977980e20d03d819bd
unknown <bogdanlikuliks@hotmail.com>
2020-01-12 13:23:07 +0100

    ver 1.13 - adding changing some css

--- src/App.js
+++ src/App.js
@@ -72,41 +72,45 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Hello</h1>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler} >Toggle Persons</button>
-        {/* onClick={() => this.switchNameHandler('Maximilian!!')} >Switch Name</button> */}
-        {/* <button onClick={() => {return this.switchNameHandler()}} >Switch Name</button> */}
-        {this.state.showPersons === true ?
-          <div>
+        <div className='MainForPerson'>
+          <h1>Hello</h1>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler} >Toggle Persons</button>
+          {/* onClick={() => this.switchNameHandler('Maximilian!!')} >Switch Name</button> */}
+          {/* <button onClick={() => {return this.switchNameHandler()}} >Switch Name</button> */}
+          {this.state.showPersons === true ?
+            <div>
 
-            <Person
-              changed={this.nameChangeHandler}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}>My Hobby:</Person>
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age} />
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div> : null
-        }
-        {/* <button onClick={this.changeFriendNameHandler} >Friend Name Change</button>
-        <Friend
-          click={this.changeFriendNameHandler}
-          name={this.state.friends[0].name}
-          age={this.state.friends[0].age}>My Interests:</Friend>
-        <Friend
-          name={this.state.friends[1].name}
-          age={this.state.friends[1].age} /> */}
-      </div>
+              <Person
+                changed={this.nameChangeHandler}
+                click={this.switchNameHandler.bind(this, 'Max!')}
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age}>My Hobby:</Person>
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age} />
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age} />
+            </div> : null
+          }
+        </div>
+        <div className='MainForFriend'>
+          <button className='btnFriend' onClick={this.changeFriendNameHandler} >Friend Name Change</button>
+          <Friend
+            click={this.changeFriendNameHandler}
+            name={this.state.friends[0].name}
+            age={this.state.friends[0].age}>My Interests:</Friend>
+          <Friend
+            name={this.state.friends[1].name}
+            age={this.state.friends[1].age} />
+        </div>
+      </div> // <div className="App">
     );
   }
 }
 
 export default App;
 
-// ver 1.11 - 49. Working with Inline Styles
+// ver 1.12 - 53. Rendering Content Conditionally
